afe71b7e02f89b50d717bc68fe6bab7889ac7d19
shai <shai88950@students.sbcsc.k12.in.us>
2023-01-13 13:44:01 -0500

    y

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -11,5 +11,20 @@ public class LetterFrequencies
 		System.out.println("The following shows the letter frequencies for the phrase");
 
     /* your code here */
-	}
+	for (int i = 0; i < 26; i++){
+
+		String let = letters.substring(i,i +1);
+		int co = 0;
+		
+		for (int q = 0; q < phrase.length(); q++){
+			String con = phrase.substring(q,q+1);
+			
+			if (let.equals(con)){
+				co++;
+			}
+		}
+	
+	System.out.println(let + " appears " + co + " times.");
+	}	
+}
 }
